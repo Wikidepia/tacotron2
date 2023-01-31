5ab9a9e4f4e3f49f47167dd70c70c3f233e6fbb1
Akmal <magchuz@pm.me>
2023-01-31 14:51:06 +0700

    Update new symbols

--- text/symbols.py
+++ text/symbols.py
@@ -5,7 +5,7 @@ Defines the set of symbols used in text input to the model.
 The default is a set of ASCII characters that works well for English or text that has been run through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details. '''
 _pad        = '_'
 _punctuation = ',.? '
-_letters = 'ŋadgtəʃjkɛblmipʒɲwsfnzrɔuoxʔh'
+_letters = 'abdefhijklmnoprstuwxzŋɔəɛɡɪɲʃʊʒʔ'
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 
